83dee2eb1a23708d0ad04a580d17797da9c3b6f3
Colin Middleton <microsoft@medievalresearch.co.uk>
2024-11-09 19:48:53 +0000

    Split shared files out of the Senbrick systems.

---
+++ Enums/ProductProperties.cs
@@ -0,0 +1,45 @@
+﻿namespace CM.SendBrickShared.Enums
+{
+    /// <summary>
+    /// Properties for the items.
+    /// </summary>
+    public class ProductProperties
+    {
+        public enum IdType
+        {
+            Unset,
+            BoId,           // inventory_id
+            BrickLinkId,    // used by BrickLink
+            BrickOwlId,     // used by BrickOwl
+            ExternalId,
+            LegoId,
+            LotId,          // bind_id
+            LocalId         // used by local system
+        }
+        public enum Condition
+        {
+            NoUpdate,
+            New,            // new_or_used = "N": New, completeness = "C": Complete
+            NewSealed,      // new_or_used = "N": New, completeness = "S": Sealed
+            NewComplete,    // new_or_used = "N": New, completeness = "C": Complete
+            NewIncomplete,  // new_or_used = "N": New, completeness = "B": Incomplete
+            UsedComplete,   // new_or_used = "U": Used, completeness = "C": Complete
+            UsedIncomplete, // new_or_used = "U": Used, completeness = "B": Incomplete
+            UsedLikeNew,    // new_or_used = "U": Used, completeness = "C": Complete
+            UsedGood,       // new_or_used = "U": Used, completeness = "C": Complete
+            UsedAcceptable, // new_or_used = "U": Used, completeness = "C": Complete
+            Other           // new_or_used = "U": Used, completeness = "B": Incomplete
+        }
+        public enum Category
+        {
+            Gear,
+            Instructions,
+            Minibuild,
+            Minifigure,
+            Packaging,
+            Part,
+            Sticker,
+            Unknown
+        }
+    }
+}
---
+++ Enums/SystemTypes.cs
@@ -0,0 +1,27 @@
+﻿namespace CM.SendBrickShared.Enums
+{
+    /// <summary>
+    /// Systems that we can connect to.
+    /// </summary>
+    public class SystemTypes
+    {
+        // All the systems that can send Orders through this system.
+        public enum SourceSystemType
+        {
+            BrickLink,
+            BrickOwl,
+            Local,
+            Unset
+        }
+
+        // All the systems that can be used to Post Orders through this system.
+        public enum PostageSystemType
+        {
+            DPD,
+            Evri,
+            RoyalMail,
+            UPS,
+            Unset
+        }
+    }
+}
